22158be2df6f44820bd803c74d44cc42663149aa
Luna <git@l4.pm>
2021-01-08 14:34:45 -0300

    add specs for time invalidation and clean-when-full

--- spec/expiring_hash_spec.cr
+++ spec/expiring_hash_spec.cr
@@ -9,4 +9,25 @@ describe ExpiringHash do
     hash[1].should eq 2
     hash[2].should eq 3
   end
+
+  it "invalidates" do
+    hash = ExpiringHash(Int32, Int32).new(10, 1.seconds)
+    hash[1] = 2
+
+    sleep 1.seconds
+
+    hash[1]?.should eq nil
+  end
+
+  it "cleans when full" do
+    hash = ExpiringHash(Int32, Int32).new(1, 1.seconds)
+    hash[1] = 2
+    hash[1].should eq 2
+
+    sleep 1.seconds
+
+    hash[2] = 3
+    hash[1]?.should eq nil
+    hash[2].should eq 3
+  end
 end
